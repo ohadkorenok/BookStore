58b56a571e73ef67fe57c95deeb5268950bcf0d7
koreo@wincs.cs.bgu.ac.il <ohadkorenok@gmail.com>
2018-11-22 21:24:38 +0200

    initial commit.

---
+++ src/tests/java/FutureTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FutureTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void resolve() {
+    }
+
+    @Test
+    public void isDone() {
+    }
+
+    @Test
+    public void get1() {
+    }
+}
