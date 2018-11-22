7dad698aae3777b137bc2d91c946c536476c1101
koreo@wincs.cs.bgu.ac.il <ohadkorenok@gmail.com>
2018-11-22 20:49:52 +0200

    initial commit.

---
+++ src/tests/java/MessageBusImplTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageBusImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void subscribeEvent() {
+    }
+
+    @Test
+    public void subscribeBroadcast() {
+    }
+
+    @Test
+    public void complete() {
+    }
+
+    @Test
+    public void sendBroadcast() {
+    }
+
+    @Test
+    public void sendEvent() {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void unregister() {
+    }
+
+    @Test
+    public void awaitMessage() {
+    }
+}
