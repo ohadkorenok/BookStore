9bd2026dd087f21db42a62a2d5bf75e5d11f7ef4
nitzan <nitzancch@gmail.com>
2018-12-11 13:25:29 +0200

    New-Update

--- src/main/java/bgu/spl/mics/application/Messages/BookOrderEvent.java
+++ src/main/java/bgu/spl/mics/application/Messages/BookOrderEvent.java
@@ -6,12 +6,10 @@ import bgu.spl.mics.application.passiveObjects.Customer;
 public class BookOrderEvent implements Event {
     private Customer c1;
     private int tick;
-    private int price;
     private String name;
-    public BookOrderEvent(Customer c1,int startOfCareTick,int price,String name){this.c1=c1;this.tick=startOfCareTick;this.price=price;this.name=name;}
+    public BookOrderEvent(Customer c1,int startOfCareTick,int price,String name){this.c1=c1;this.tick=startOfCareTick;this.name=name;}
 
     public Customer getCustomer(){return c1;}
     public int getissuedTick(){return tick;}
-    public int getPrice(){return price;}
     public String getName(){return name;}
 }
